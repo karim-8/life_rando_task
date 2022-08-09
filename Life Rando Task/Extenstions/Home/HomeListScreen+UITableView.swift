9952b08f48638cc966baa87144cc9e34ded48dc8
Karim <kariimAhmed8@gmail.com>
2022-08-09 22:30:44 +0200

    Update Extension

---
+++ Life Rando Task/Extenstions/Home/HomeListScreen+UITableView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeListScreen+UITableView.swift
+//  Life Rando Task
+//
+//  Created by KarimAhmed on 09/08/2022.
+//
+
+import Foundation
+import UIKit
+
+extension ListHomeScreen:UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel?.searched.count ?? 0
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantCell") as? RestaurantCell
+        cell?.viewModel = viewModel?.viewModelForCell(at: indexPath.row)
+        cell?.configure()
+        return cell ?? UITableViewCell()
+    }
+}
