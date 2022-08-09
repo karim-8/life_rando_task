48dcd530953a2f22838a19789a990edfea495f54
Karim <kariimAhmed8@gmail.com>
2022-08-09 21:01:04 +0200

    Adding project files

---
+++ Life Rando TaskUITests/Life_Rando_TaskUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Life_Rando_TaskUITestsLaunchTests.swift
+//  Life Rando TaskUITests
+//
+//  Created by KarimAhmed on 09/08/2022.
+//
+
+import XCTest
+
+class Life_Rando_TaskUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
