57ba3501d9d2c147813fb0e4eeeeefbe80d3e4d8
Karim <kariimAhmed8@gmail.com>
2022-08-09 22:30:17 +0200

    Adding Filter Module

---
+++ Life Rando Task/Filter View/FilterCoordinator.swift
@@ -0,0 +1,20 @@
+//
+//  FilterCoordinator.swift
+//  Life Rando Task
+//
+//  Created by KarimAhmed on 09/08/2022.
+//
+
+import Foundation
+import UIKit
+
+protocol FilterCoordinatorProtocol {
+    func navigateTo(objects:[RestaurantDetails],view:UIViewController)
+}
+
+class FilterViewCoordinator: CoordinatorProtocol {
+    //MARK:- NAVIGATE TO
+    func navigateTo(view: UIViewController) {
+        
+    }
+}
